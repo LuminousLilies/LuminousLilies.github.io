ab2bb1bfe159d4b5419c6847c98fe689773fd705
LuminousLilies <LuminousLily@zoho.com>
2020-05-22 17:39:14 -0700

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://LuminousLilies.github.io',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'LuminousLilies',
+  projectName: 'LuminousLilies.github.io',
   themeConfig: {
     navbar: {
       title: 'My Site',
