c0b5b125be55e1028aeefab3560506425ac87b17
LuminousLilies <LuminousLily@zoho.com>
2020-06-21 13:20:29 -0700

    update: sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,7 @@
 module.exports = {
   someSidebar: {
-    Breakfast: ["breakfast/mochiko-pancakes", "breakfast/simple-granola"]
+    Breakfast: ["breakfast/mochiko-pancakes", "breakfast/simple-granola"],
+    Dough: ["dough/sourdough-pizza", "dough/sourdough-tortilla"],
+    Sauces: ["sauces/spicy-pizza-sauce"]
   },
 };
