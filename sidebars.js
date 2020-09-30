fbf2d630e8de811f03f1c98e5ad3fbcc8bd5bd3d
LuminousLilies <LuminousLily@zoho.com>
2020-09-29 22:51:07 -0700

    update sidebar - marinara sauce

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     Breakfast: ["breakfast/mochiko-pancakes", "breakfast/simple-granola"],
     Dough: ["dough/sourdough-pizza", "dough/sourdough-tortilla"],
-    Sauces: ["sauces/spicy-pizza-sauce"]
+    Sauces: ["sauces/spicy-pizza-sauce","sauces/marinara-sauce"]
   },
 };
