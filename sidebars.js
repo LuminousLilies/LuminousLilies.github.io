4196d82f3d4a955c7f05838d16fbea90c1577307
LuminousLilies <LuminousLily@zoho.com>
2021-05-29 17:10:24 -0700

    updates

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,9 @@
 module.exports = {
   someSidebar: {
-    Breakfast: ["breakfast/mochiko-pancakes", "breakfast/simple-granola"],
+    Breakfast: ["breakfast/simple-granola","breakfast/mochiko-pancakes", "breakfast/simple-granola"],
+    Dessert: ["dessert/irish-soda-farls","dessert/vegan-oatmeal-cookies", "dessert/vegan-snickerdoodles"],
     Dough: ["dough/sourdough-pizza", "dough/sourdough-tortilla"],
+    Mains: ["mains/sourdough-calzone"],
     Sauces: ["sauces/spicy-pizza-sauce","sauces/marinara-sauce"]
   },
 };
